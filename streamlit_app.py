5ae13adcbdccd386d13c70707abc2ed3468c201c
anikale97 <127396371+anikale97@users.noreply.github.com>
2023-03-11 23:58:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
+# put the data into a dataframe
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
-streamlit.write(df)
+# streamlit.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
 # print(color_list)
